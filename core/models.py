51c9aa54cf9a9e0431d43fb97ce78dc51537c4f1
Batuhan Gurbuz <batuhang002@gmail.com>
2024-01-04 13:52:31 +0300

    feat: add general settings models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class GeneralSetting(models.Model):
+    name = models.CharField(
+        default = '',
+        max_length = 255,
+        blank = True,
+    )
+    
+    description = models.CharField(
+        default = '',
+        max_length = 255,
+        blank = True,
+    )
+    
+    parameter = models.CharField(
+        default = '',
+        max_length = 255,
+        blank = True,
+    )
+    
+    updatedDate = models.DateField(
+        blank = True,
+        auto_now = True
+    )
+    
+    updatedDate = models.DateField(
+        blank = True,
+        auto_now_add = True
+    )
